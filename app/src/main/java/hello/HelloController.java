41d0eabf745d0e4dc64de1014ca5abd2fdca15f4
Raj <53164841+jammanaraj@users.noreply.github.com>
2022-06-21 13:32:36 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot! mardal darling welcome to home";
+        return "Hello from Spring Boot!";
     }   
 }
